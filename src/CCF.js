4e3d141c1856506df7d0b9f5f082daa534f70ffc
Rodrigo Poma <rodpoma@gmail.com>
2020-11-05 10:28:54 -0500

    phone landing and mobile menu set up

--- src/CCF.js
+++ src/CCF.js
@@ -2,21 +2,35 @@ import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
+import MobileMenu from "./components/MobileMenu";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import {BrowserRouter, Route} from "react-router-dom";
+import {MDBContainer} from "mdbreact";
+
 import "./CCF.css";
+import "mdbreact/dist/css/mdb.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const CCF = () => {
   return (
     <BrowserRouter>
       <Navigation />
-      <div>
+      <MDBContainer
+        fluid
+        style={{
+          backgroundColor: "#0b0633",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/products" component={Products} />
-      </div>
+        <Route path="/mobileMenu" component={MobileMenu} />
+      </MDBContainer>
     </BrowserRouter>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import {MDBCol, MDBContainer, MDBAnimation} from "mdbreact";
+import logo from "../assets/Original on Transparent.png";
 
-const Home = () => {
-  return <div>home</div>;
+const Landing = () => {
+  return (
+    <MDBContainer className="cards" fluid>
+      <MDBCol className="white-text text-center mt-5">
+        <MDBAnimation type="fadeIn" delay=".6s" className="flex-center">
+          <img src={logo} alt="logo" className="logo" style={{width: "80vw"}} />
+        </MDBAnimation>
+        <hr className="hr-light my-4 w-75" />
+        <MDBAnimation type="fadeIn" delay="1.2s">
+          <em>Where Dreams Come true</em>
+        </MDBAnimation>
+      </MDBCol>
+    </MDBContainer>
+  );
 };
 
-export default Home;
+export default Landing;
