4bc3aebc15b11be5948b92dda6771d709f6a63f8
Rodrigo Poma <rodpoma@gmail.com>
2020-12-03 16:40:42 -0600

    emailjs not optimal for outlook, find another method

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,7 +6,6 @@ import {
   MDBCardText,
   MDBCardTitle,
   MDBCardBody,
-  MDBCardImage,
   MDBCardGroup,
 } from "mdbreact";
 import MediaQuery from "react-responsive";
@@ -44,7 +43,9 @@ const Products = () => {
             </p>
           </MDBAnimation>
           <MDBAnimation type="fadeIn" delay=".3s">
-            <h4 className="white-text text-center mt-5">Vendor finance</h4>
+            <h4 className="white-text text-center mt-5">
+              Vendor finance - Customer Financing Solutions
+            </h4>
           </MDBAnimation>
           <MDBAnimation type="fadeIn" delay=".6s">
             <p
@@ -63,62 +64,98 @@ const Products = () => {
       </MediaQuery>
       {/*Web Browser*/}
       <MediaQuery minDeviceWidth={1201}>
-        <h1 className="white-text">Products</h1>
-        <hr className="hr-light my-4" />
-        <MDBCardGroup>
-          <MDBCard>
-            <MDBCardImage
-              src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg"
-              alt="MDBCard image cap"
-              top
-              hover
-              overlay="white-slight"
-            />
-            <MDBCardBody>
-              <MDBCardTitle tag="h5">PE Owned Businesses</MDBCardTitle>
-              <MDBCardText>
-                <p>
-                  As an Independent equipment finance company, our focus is on
-                  providing capital to Private Equity Owned and Sponsored
-                  businesses to purchase equipment. In-addition we can Sale
-                  Lease Back, existing equipment, releasing liquidity for
-                  additional investments.
-                  <br /> Cape focuses on EBITDA as a measurement, not the more
-                  historical measurement of pre-tax profit driven by traditional
-                  bank leasing companies.
-                  <br /> If your business is private, non-rated, middle-market
-                  we can help assist you in financing:
-                  <br />
-                  - New equipment purchases.
-                  <br />
-                  - Credit lines for future equipment needs.
-                  <br />- Recapitalizations of existing equipment.
-                </p>
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
+        <MDBAnimation type="fadeIn" delay=".3s">
+          <h1 className="white-text">Products</h1>
+          <hr className="hr-light my-4" />
+          <MDBCardGroup>
+            <MDBCard>
+              <MDBCardBody>
+                <MDBCardTitle tag="h5">PE Owned Businesses</MDBCardTitle>
+                <MDBCardText>
+                  <p>
+                    As an Independent equipment finance company, our focus is on
+                    providing capital to Private Equity Owned and Sponsored
+                    businesses to purchase equipment. In-addition we can Sale
+                    Lease Back, existing equipment, releasing liquidity for
+                    additional investments.
+                    <br /> Cape focuses on EBITDA as a measurement, not the more
+                    historical measurement of pre-tax profit driven by
+                    traditional bank leasing companies.
+                    <br /> If your business is private, non-rated, middle-market
+                    we can help assist you in financing:
+                    <br />
+                    - New equipment purchases.
+                    <br />
+                    - Credit lines for future equipment needs.
+                    <br />- Recapitalizations of existing equipment.
+                  </p>
+                </MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
 
-          <MDBCard>
-            <MDBCardImage
-              src="https://mdbootstrap.com/img/Photos/Others/images/48.jpg"
-              alt="MDBCard image cap"
-              top
-              hover
-              overlay="white-slight"
-            />
-            <MDBCardBody>
-              <MDBCardTitle tag="h5">Vendor Finance</MDBCardTitle>
-              <MDBCardText>
-                Cape provides financing programs to equipment manufacturers. As
-                an Independent we are able to support your customers across the
-                credit spectrum and transaction size, this allows manufacturers
-                to partner with one financing sources rather than several. By
-                partnering with Cape, manufacturers are able to access different
-                customer financing products.
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCardGroup>
+            <MDBCard>
+              <MDBCardBody>
+                <MDBCardTitle tag="h5">
+                  Vendor finance - Customer Financing Solutions
+                </MDBCardTitle>
+                <MDBCardText>
+                  Cape provides financing programs to equipment manufacturers.
+                  As an Independent we are able to support your customers across
+                  the credit spectrum and transaction size, this allows
+                  manufacturers to partner with one financing sources rather
+                  than several. By partnering with Cape, manufacturers are able
+                  to access different customer financing products.
+                </MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCardGroup>
+          <MDBContainer className="mt-5">
+            <h3 className="white-text">Recent Transactions</h3>
+            <hr className="hr-light my-4" />
+            <MDBCardGroup className="mt-5">
+              <MDBCard>
+                <MDBCardBody>
+                  <MDBCardTitle className="black-text" tag="h5">
+                    $6,700,000 for New Machine Tools
+                  </MDBCardTitle>
+                  <MDBCardText>
+                    <br />
+                    -AeroSpace & Defense Supplier
+                    <br /> -COVID-19 Impacted Financial Performance
+                    <br /> -48 Month $1 buyout lease
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+              <MDBCard>
+                <MDBCardBody>
+                  <MDBCardTitle className="black-text" tag="h5">
+                    $3,500,000 for New Plastic Injection Molding Machine
+                  </MDBCardTitle>
+                  <MDBCardText>
+                    <br />
+                    -Recent Start-Up
+                    <br />
+                    -Required additional collateral and personal guaranty
+                    <br /> -36 Month $1 buyout lease
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+              <MDBCard>
+                <MDBCardBody>
+                  <MDBCardTitle className="black-text" tag="h5">
+                    $2,000,000 for Food Production Equipment
+                  </MDBCardTitle>
+                  <MDBCardText>
+                    <br />
+                    -Recent Acquisition
+                    <br /> -Debt 6X EBITDA
+                    <br /> -36 Month $1 buyout lease
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCardGroup>
+          </MDBContainer>
+        </MDBAnimation>
       </MediaQuery>
     </MDBContainer>
   );
